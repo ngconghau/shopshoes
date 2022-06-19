3434f535efd12bd66c59f8d1e0f5181b86d98f7c
nguyenconghau <ncconghau@gmail.com>
2022-06-20 05:46:02 +0700

    second commit

--- shopshoes/settings.py
+++ shopshoes/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
-SESSION_EXPIRE_SECONDS = 60
+SESSION_EXPIRE_SECONDS = 7200
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
 SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
